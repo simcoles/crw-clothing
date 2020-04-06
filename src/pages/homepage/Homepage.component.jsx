58adce218edbb44274714e83dc222de98fbd621d
Simeon Coles <sim.coles67@gmail.com>
2020-04-06 14:36:22 +0100

    update our menu-items to navigate withRouter HOC

--- src/pages/homepage/Homepage.component.jsx
+++ src/pages/homepage/Homepage.component.jsx
@@ -1,7 +1,7 @@
 import React from "react";
+import { Route } from "react-router-dom";
 import "./homepage.styles.scss";
 import Directory from "../../components/directory/Directory.jsx";
-import MenuItem from "../../components/menu-item/MenuItem.jsx";
 
 const Homepage = () => {
   return (
